de4ed46492b3ba9223a97593afa8eedfb5b794a3
JonhEdsonRibeirodeCa <jonh.carvalho@prof.infnet.edu.br>
2023-03-20 07:26:41 -0300

    Add project files.

---
+++ AzureBlobStorage.WebApi/Program.cs
@@ -0,0 +1,30 @@
+using AzureBlobStorage.WebApi.Repository;
+using AzureBlobStorage.WebApi.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddTransient<IAzureStorage, AzureStorage>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
